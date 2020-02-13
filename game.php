0cf18a468f732989d4632412972c9b38bf253823
Thibence <salencethibaut95@gmail.com>
2020-02-13 14:11:46 +0100

    jesus christ how will i ever survive the internship, let alone survive long enough to get there

---
+++ game.php
@@ -0,0 +1,28 @@
+<?php
+
+new blackjack {}
+
+
+?>
+<!DOCTYPE>
+<html>
+<head>
+    <meta charset="UTF-8" lang="eng">
+</head>
+
+
+<body>
+<header>
+    <h1>BJ</h1>
+</header>
+<div id="score">
+    <p>Your score: <?php echo ?></p>
+</div>
+<div id="buttons">
+    <button name="hit">Hit me!</button>
+    <button name="stand">Stand</button>
+    <button name="wuss">Surrender</button>
+</div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,21 +1,36 @@
 <?php
 
-/*$cards = array('2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, 'T' => 10, 'J' => 10, 'Q' => 10, 'K' => 10, 'A' => 11);
-$ctypes = array('C', 'H', 'D', 'S');*/
+declare(strict_types = 1);
 
+/*ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);*/
+
+/*$cards = array('2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, 'T' => 10, 'J' => 10, 'Q' => 10, 'K' => 10, 'A' => 11);
+$types = array('C', 'H', 'D', 'S');*/
+
+session_start();
+
+//THIS IS A CLASS
 class blackjack {
-    function hit($hit) {
+    //properties:
+    public $score = 0;
+
+    //methods:
+    function hit() {
+        $randnum = rand(1, 11);
+        $this->score = $this->score += $randnum;
         
     }
 
-    function stand($stand) {
+    function stand() {
 
     }
 
-    function surrender($wuss) {
+    function surrender() {
 
     }
 }
 
 
-require 'bj-game.php';
+require 'game.php';
